c783c44d81f6b77477a784bfdc26e960a4b71a02
Isaac Miti <ikayz@live.com>
2026-03-05 20:16:00 +0200

    partials added

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import { fileURLToPath } from 'url';
 import path from 'path';
 import express from 'express';
+import { setDefaultAutoSelectFamily } from 'net';
 
 // Define the the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -19,6 +20,13 @@ const app = express();
 
 // Serve static files from the public directory
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Set EJS as the templating engine
+app.set('view engine', 'ejs');
+
+// Tell Express where to find your templates
+app.set('views', path.join(__dirname, 'src/views'));
+setDefaultAutoSelectFamily;
 
 /**
  * Routes
